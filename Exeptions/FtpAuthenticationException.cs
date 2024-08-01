530fe57bd1671dba5fe9d9ab08a358b8661d7c59
Jorge Miguel Mendoza Correa <jorge.mendoza@bdcloud.onmicrosoft.com>
2024-08-01 13:30:39 -0600

    Add  Exeptions

---
+++ Exeptions/FtpAuthenticationException.cs
@@ -0,0 +1,12 @@
+﻿namespace Red.FTP.Exemptions;
+
+public class FtpAuthenticationException : Exception
+{
+    public FtpAuthenticationException() { }
+
+    public FtpAuthenticationException(string message)
+        : base(message) { }
+
+    public FtpAuthenticationException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
