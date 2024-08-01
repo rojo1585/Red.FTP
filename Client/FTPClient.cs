e7e9da9c59f4d29a8488ef0b9f12a30a24e24b8a
Jorge Miguel Mendoza Correa <jorge.mendoza@bdcloud.onmicrosoft.com>
2024-08-01 13:29:55 -0600

    Add validation for authentication

--- Client/FTPClient.cs
+++ Client/FTPClient.cs
@@ -1,4 +1,6 @@
-﻿using Red.FTP.Helpers;
+﻿using Red.FTP.Exemptions;
+using Red.FTP.Handler;
+using Red.FTP.Helpers;
 using Red.FTP.Interfaces;
 using Red.FTP.Models;
 using System.Net.Sockets;
@@ -11,30 +13,46 @@ public class FtpClient(IFtpCommand _commands, IFtpDataTransfer _ftpDataTransfer)
     private TcpClient? _controlClient;
     private BasicFtpCredentials? _auth;
     private bool _disposed = false;
+    private bool isLogin = false;
 
 
     public void SetCredentials(string user, string password) =>
         _auth = new BasicFtpCredentials(user, password);
 
-    public async Task<string> CreateConnectionAsync(string host, int port = 21)
+    public async Task<FtpResponse> CreateConnectionAsync(string host, int port = 21)
     {
         _controlClient = new TcpClient();
         await _controlClient.ConnectAsync(host, port);
         _commands.SetNetworkStream(_controlClient.GetStream());
-        return await _commands.ReadResponseAsync();
+        var response = await _commands.ReadResponseAsync();
+
+        var (statusCode, description) = GetStatusCodeAndMessage(response);
+        return new(statusCode, description);
     }
 
-    public async Task<string> AuthAsync()
+    public async Task<FtpResponse> AuthAsync()
     {
         if (_auth is null or { Password: null, User: null }) throw new InvalidOperationException("Credentials not set.");
+
         _commands.SendCommand($"USER {_auth.User}");
         await _commands.ReadResponseAsync();
         _commands.SendCommand($"PASS {_auth.Password}");
-        return await _commands.ReadResponseAsync();
+        string response = await _commands.ReadResponseAsync();
+
+        var (statusCode, description) = GetStatusCodeAndMessage(response);
+
+        if (FtpStatusCodes.IsAuthenticatedStatusCode(statusCode))
+        {
+            isLogin = true;
+        }
+        return new(statusCode, description);
     }
 
     public async Task<IEnumerable<FtpFile>> GetFilesAsync(string path)
     {
+        if (!isLogin)
+            throw new FtpAuthenticationException("User not login");
+
         var (ip, port) = await GetPasiveConnection();
         return await _ftpDataTransfer.GetFilesAsync(path, ip, port);
     }
@@ -45,6 +63,22 @@ public class FtpClient(IFtpCommand _commands, IFtpDataTransfer _ftpDataTransfer)
         string response = await _commands.ReadResponseAsync();
         return FtpResponseParser.ParsePasiveResponse(response);
     }
+
+    private static (int statusCode, string description) GetStatusCodeAndMessage(string response)
+    {
+        if (response.Length < 3)
+            throw new InvalidOperationException("Invalid FTP response format.");
+
+        if (int.TryParse(response.AsSpan(0, 3), out int statusCode))
+        {
+            string message = FtpStatusCodes.GetStatusCodeDescription(statusCode);
+            return (statusCode, message);
+        }
+
+        throw new InvalidOperationException("Invalid FTP response format.");
+    }
+
+
     public void Dispose()
     {
         Dispose(true);
