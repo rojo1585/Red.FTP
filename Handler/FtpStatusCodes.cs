dd8d160636131140a6a15930bf6a8f8b97d92a7a
Jorge Miguel Mendoza Correa <jorge.mendoza@bdcloud.onmicrosoft.com>
2024-08-06 11:05:49 -0600

    Modify status codes

--- Handler/FtpStatusCodes.cs
+++ Handler/FtpStatusCodes.cs
@@ -1,78 +1,80 @@
-﻿namespace Red.FTP.Handler;
+﻿using static Red.FTP.Models.Literals;
+
+namespace Red.FTP.Handler;
 
 internal static class FtpStatusCodes
 {
-    private static readonly Dictionary<int, string> _statusCodes = new()
-    {
-        { 110, "Restart marker reply" },
-        { 120, "Service ready in nnn minutes" },
-        { 125, "Data connection already open; transfer starting" },
-        { 150, "File status okay; about to open data connection" },
-        { 200, "Command okay" },
-        { 202, "Command not implemented, superfluous at this site" },
-        { 211, "System status, or system help reply" },
-        { 212, "Directory status" },
-        { 213, "File status" },
-        { 214, "Help message" },
-        { 215, "NAME system type, where NAME is an official system name from the list in the Assigned Numbers document" },
-        { 220, "Service ready for new user" },
-        { 221, "Service closing control connection. Logged out if appropriate" },
-        { 225, "Data connection open; no transfer in progress" },
-        { 226, "Closing data connection. Requested file action successful (for example, file transfer or file abort)" },
-        { 227, "Entering Passive Mode (h1,h2,h3,h4,p1,p2)" },
-        { 229, "Extended passive mode entered" },
-        { 230, "User logged in, proceed" },
-        { 232, "User logged in, authorized by security data exchange" },
-        { 234, "Security data exchange complete" },
-        { 235, "Security data exchange completed successfully" },
-        { 250, "Requested file action okay, completed" },
-        { 257, "\"PATHNAME\" created" },
-        { 331, "User name okay, need password" },
-        { 332, "Need account for login" },
-        { 334, "Requested security mechanism ok" },
-        { 335, "Security data is acceptable. More data is required to complete the security data exchange" },
-        { 336, "Username okay, need password" },
-        { 350, "Requested file action pending further information" },
-        { 421, "Service not available, closing control connection. This may be a reply to any command if the service knows it must shut down" },
-        { 425, "Cannot open data connection" },
-        { 426, "Connection closed; transfer aborted" },
-        { 431, "Need some unavailable resource to process security" },
-        { 450, "Requested file action not taken. File unavailable (for example, file busy)" },
-        { 451, "Requested action aborted. Local error in processing" },
-        { 452, "Requested action not taken. Insufficient storage space in system" },
-        { 500, "Syntax error, command unrecognized. This may include errors such as command line too long" },
-        { 501, "Syntax error in parameters or arguments" },
-        { 502, "Command not implemented" },
-        { 503, "Bad sequence of commands" },
-        { 504, "Command not implemented for that parameter" },
-        { 521, "Data connection cannot be opened with this PROT setting" },
-        { 522, "Server does not support the requested network protocol" },
-        { 530, "Not logged in" },
-        { 532, "Need account for storing files" },
-        { 533, "Command protection level denied for policy reasons" },
-        { 534, "Request denied for policy reasons" },
-        { 535, "Failed security check (hash, sequence, and so on)" },
-        { 536, "Requested PROT level not supported by mechanism" },
-        { 537, "Command protection level not supported by security mechanism" },
-        { 550, "Requested action not taken. File unavailable" },
-        { 551, "Requested action aborted: Page type unknown" },
-        { 552, "Requested file action aborted. Exceeded storage allocation (for current directory or dataset)" },
-        { 553, "Requested action not taken. File name not allowed" },
-        { 631, "Integrity protected reply" },
-        { 632, "Confidentiality and integrity protected reply" },
-        { 633, "Confidentiality protected reply" }
+    private static readonly Dictionary<FtpStatusCode, string> _statusCodes = new()
+{
+    { FtpStatusCode.RestartMarkerReply, "Restart marker reply" },
+    { FtpStatusCode.ServiceReadyInMinutes, "Service ready in nnn minutes" },
+    { FtpStatusCode.DataConnectionAlreadyOpen, "Data connection already open; transfer starting" },
+    { FtpStatusCode.FileStatusOkay, "File status okay; about to open data connection" },
+    { FtpStatusCode.CommandOkay, "Command okay" },
+    { FtpStatusCode.CommandNotImplemented, "Command not implemented, superfluous at this site" },
+    { FtpStatusCode.SystemStatus, "System status, or system help reply" },
+    { FtpStatusCode.DirectoryStatus, "Directory status" },
+    { FtpStatusCode.FileStatus, "File status" },
+    { FtpStatusCode.HelpMessage, "Help message" },
+    { FtpStatusCode.NameSystemType, "NAME system type, where NAME is an official system name from the list in the Assigned Numbers document" },
+    { FtpStatusCode.ServiceReadyForNewUser, "Service ready for new user" },
+    { FtpStatusCode.ServiceClosingControlConnection, "Service closing control connection. Logged out if appropriate" },
+    { FtpStatusCode.DataConnectionOpen, "Data connection open; no transfer in progress" },
+    { FtpStatusCode.ClosingDataConnection, "Closing data connection. Requested file action successful (for example, file transfer or file abort)" },
+    { FtpStatusCode.EnteringPassiveMode, "Entering Passive Mode (h1,h2,h3,h4,p1,p2)" },
+    { FtpStatusCode.ExtendedPassiveModeEntered, "Extended passive mode entered" },
+    { FtpStatusCode.UserLoggedIn, "User logged in, proceed" },
+    { FtpStatusCode.UserLoggedInAuthorized, "User logged in, authorized by security data exchange" },
+    { FtpStatusCode.SecurityDataExchangeComplete, "Security data exchange complete" },
+    { FtpStatusCode.SecurityDataExchangeCompletedSuccessfully, "Security data exchange completed successfully" },
+    { FtpStatusCode.RequestedFileActionOkay, "Requested file action okay, completed" },
+    { FtpStatusCode.PathnameCreated, "Created" },
+    { FtpStatusCode.UserNameOkayNeedPassword, "User name okay, need password" },
+    { FtpStatusCode.NeedAccountForLogin, "Need account for login" },
+    { FtpStatusCode.RequestedSecurityMechanismOkay, "Requested security mechanism ok" },
+    { FtpStatusCode.SecurityDataAcceptable, "Security data is acceptable. More data is required to complete the security data exchange" },
+    { FtpStatusCode.UsernameOkayNeedPassword, "Username okay, need password" },
+    { FtpStatusCode.RequestedFileActionPendingFurtherInformation, "Requested file action pending further information" },
+    { FtpStatusCode.ServiceNotAvailable, "Service not available, closing control connection. This may be a reply to any command if the service knows it must shut down" },
+    { FtpStatusCode.CannotOpenDataConnection, "Cannot open data connection" },
+    { FtpStatusCode.ConnectionClosedTransferAborted, "Connection closed; transfer aborted" },
+    { FtpStatusCode.NeedUnavailableResource, "Need some unavailable resource to process security" },
+    { FtpStatusCode.RequestedFileActionNotTaken, "Requested file action not taken. File unavailable (for example, file busy)" },
+    { FtpStatusCode.RequestedActionAbortedLocalError, "Requested action aborted. Local error in processing" },
+    { FtpStatusCode.RequestedActionNotTakenInsufficientStorage, "Requested action not taken. Insufficient storage space in system" },
+    { FtpStatusCode.SyntaxErrorCommandUnrecognized, "Syntax error, command unrecognized. This may include errors such as command line too long" },
+    { FtpStatusCode.SyntaxErrorInParametersOrArguments, "Syntax error in parameters or arguments" },
+    { FtpStatusCode.BadSequenceOfCommands, "Bad sequence of commands" },
+    { FtpStatusCode.CommandNotImplementedForParameter, "Command not implemented for that parameter" },
+    { FtpStatusCode.DataConnectionCannotBeOpened, "Data connection cannot be opened with this PROT setting" },
+    { FtpStatusCode.ServerDoesNotSupportRequestedProtocol, "Server does not support the requested network protocol" },
+    { FtpStatusCode.NotLoggedIn, "Not logged in" },
+    { FtpStatusCode.NeedAccountForStoringFiles, "Need account for storing files" },
+    { FtpStatusCode.CommandProtectionLevelDenied, "Command protection level denied for policy reasons" },
+    { FtpStatusCode.RequestDeniedForPolicyReasons, "Request denied for policy reasons" },
+    { FtpStatusCode.FailedSecurityCheck, "Failed security check (hash, sequence, and so on)" },
+    { FtpStatusCode.RequestedProtectionLevelNotSupported, "Requested PROT level not supported by mechanism" },
+    { FtpStatusCode.CommandProtectionLevelNotSupported, "Command protection level not supported by security mechanism" },
+    { FtpStatusCode.RequestedActionNotTakenFileUnavailable, "Requested action not taken. File unavailable" },
+    { FtpStatusCode.RequestedActionAbortedPageTypeUnknown, "Requested action aborted: Page type unknown" },
+    { FtpStatusCode.RequestedFileActionAbortedExceededStorage, "Requested file action aborted. Exceeded storage allocation (for current directory or dataset)" },
+    { FtpStatusCode.RequestedActionNotTakenFileNameNotAllowed, "Requested action not taken. File name not allowed" },
+    { FtpStatusCode.IntegrityProtectedReply, "Integrity protected reply" },
+    { FtpStatusCode.ConfidentialityAndIntegrityProtectedReply, "Confidentiality and integrity protected reply" },
+    { FtpStatusCode.ConfidentialityProtectedReply, "Confidentiality protected reply" }
     };
 
-    private static readonly HashSet<int> AuthenticatedStatusCodes = new HashSet<int>
-    {
-        230, // User logged in, proceed.
-        232, // User logged in, authorized by security data exchange.
-        234, // Security data exchange complete.
-        235  // Security data exchange completed successfully.
-    };
+
+    private static readonly HashSet<FtpStatusCode> AuthenticatedStatusCodes =
+    [
+        FtpStatusCode.UserLoggedIn,
+        FtpStatusCode.UserLoggedInAuthorized,
+        FtpStatusCode.SecurityDataExchangeComplete,
+        FtpStatusCode.SecurityDataExchangeCompletedSuccessfully
+    ];
     public static string GetStatusCodeDescription(int statusCode)
     {
-        if (_statusCodes.TryGetValue(statusCode, out var description))
+        if (_statusCodes.TryGetValue((FtpStatusCode)statusCode, out string? description))
         {
             return description;
         }
@@ -80,7 +82,7 @@ internal static class FtpStatusCodes
     }
     public static bool IsAuthenticatedStatusCode(int statusCode)
     {
-        return AuthenticatedStatusCodes.Contains(statusCode);
+        return AuthenticatedStatusCodes.Contains((FtpStatusCode)statusCode);
     }
     public static (int statusCode, string description) GetStatusCodeAndMessage(string response)
     {
@@ -89,7 +91,7 @@ internal static class FtpStatusCodes
 
         if (int.TryParse(response.AsSpan(0, 3), out int statusCode))
         {
-            string message = FtpStatusCodes.GetStatusCodeDescription(statusCode);
+            string message = GetStatusCodeDescription(statusCode);
             return (statusCode, message);
         }
 
---
+++ Models/Literals.cs
@@ -0,0 +1,77 @@
+﻿namespace Red.FTP.Models;
+
+public static class Literals
+{
+    public enum FtpStatusCode
+    {
+        // 1xx: Positive Preliminary reply
+        RestartMarkerReply = 110,
+        ServiceReadyInMinutes = 120,
+        DataConnectionAlreadyOpen = 125,
+        FileStatusOkay = 150,
+
+        // 2xx: Positive Completion reply
+        CommandOkay = 200,
+        CommandNotImplemented = 202,
+        SystemStatus = 211,
+        DirectoryStatus = 212,
+        FileStatus = 213,
+        HelpMessage = 214,
+        NameSystemType = 215,
+        ServiceReadyForNewUser = 220,
+        ServiceClosingControlConnection = 221,
+        DataConnectionOpen = 225,
+        ClosingDataConnection = 226,
+        EnteringPassiveMode = 227,
+        ExtendedPassiveModeEntered = 229,
+        UserLoggedIn = 230,
+        UserLoggedInAuthorized = 232,
+        SecurityDataExchangeComplete = 234,
+        SecurityDataExchangeCompletedSuccessfully = 235,
+        RequestedFileActionOkay = 250,
+        PathnameCreated = 257,
+
+        // 3xx: Positive Intermediate reply
+        UserNameOkayNeedPassword = 331,
+        NeedAccountForLogin = 332,
+        RequestedSecurityMechanismOkay = 334,
+        SecurityDataAcceptable = 335,
+        UsernameOkayNeedPassword = 336,
+        RequestedFileActionPendingFurtherInformation = 350,
+
+        // 4xx: Transient Negative Completion reply
+        ServiceNotAvailable = 421,
+        CannotOpenDataConnection = 425,
+        ConnectionClosedTransferAborted = 426,
+        NeedUnavailableResource = 431,
+        RequestedFileActionNotTaken = 450,
+        RequestedActionAbortedLocalError = 451,
+        RequestedActionNotTakenInsufficientStorage = 452,
+
+        // 5xx: Permanent Negative Completion reply
+        SyntaxErrorCommandUnrecognized = 500,
+        SyntaxErrorInParametersOrArguments = 501,
+        CommandNotImplementeds = 502,
+        BadSequenceOfCommands = 503,
+        CommandNotImplementedForParameter = 504,
+        DataConnectionCannotBeOpened = 521,
+        ServerDoesNotSupportRequestedProtocol = 522,
+        NotLoggedIn = 530,
+        NeedAccountForStoringFiles = 532,
+        CommandProtectionLevelDenied = 533,
+        RequestDeniedForPolicyReasons = 534,
+        FailedSecurityCheck = 535,
+        RequestedProtectionLevelNotSupported = 536,
+        CommandProtectionLevelNotSupported = 537,
+        RequestedActionNotTakenFileUnavailable = 550,
+        RequestedActionAbortedPageTypeUnknown = 551,
+        RequestedFileActionAbortedExceededStorage = 552,
+        RequestedActionNotTakenFileNameNotAllowed = 553,
+
+        // 6xx: Protected reply
+        IntegrityProtectedReply = 631,
+        ConfidentialityAndIntegrityProtectedReply = 632,
+        ConfidentialityProtectedReply = 633
+    }
+
+}
