3d22a274dec4156175c90248dc48ab65ea8eedbd
Jorge Miguel Mendoza Correa <jorge.mendoza@bdcloud.onmicrosoft.com>
2024-08-06 13:59:05 -0600

    add new generic Exception

---
+++ Exceptions/FtpException.cs
@@ -0,0 +1,12 @@
+﻿namespace Red.FTP.Exceptions;
+
+public class FtpException : Exception
+{
+    public FtpException() { }
+
+    public FtpException(string message)
+        : base(message) { }
+
+    public FtpException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
